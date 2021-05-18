191ac5d26f84253547d26cb2e178839d05f5d01a
Shahari93 <shaharilani93@gmail.com>
2021-05-18 23:04:17 +0300

    State machine

--- Text101/Assets/Scripts/QuestGame.cs
+++ Text101/Assets/Scripts/QuestGame.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,4 +13,44 @@ public class QuestGame : MonoBehaviour
         state = startState; // setting the state to be the first state
         textComponent.text = state.GetStateStory(); // setting the text component from what we wrote in the SO, to be shown inside Unity
     }
+
+    private void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GoToNextState();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeStateAndShowText(0, nextStates);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeStateAndShowText(1, nextStates);
+        }
+
+        if (nextStates.Length <= 2)
+        {
+            return;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                ChangeStateAndShowText(2, nextStates);
+            }
+        }
+    }
+
+    // A method that have 2 parameters. One is the state num, and the second is the states array
+    private void ChangeStateAndShowText(int stateNum, State[] nextStates)
+    {
+        state = nextStates[stateNum];
+        textComponent.text = state.GetStateStory();
+    }
+
 }
