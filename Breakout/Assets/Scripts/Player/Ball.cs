a983d0728e083776b0f33334fba7e34eefd9e5a3
Shahari93 <shaharilani93@gmail.com>
2021-05-27 01:47:48 +0300

    Start of the game
    - Ball is stuck to paddle
    - When pressing Left mouse button, ball is launch to the air

--- Breakout/Assets/Scripts/Player/Ball.cs
+++ Breakout/Assets/Scripts/Player/Ball.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Config parm")]
+    [SerializeField] Paddle paddle = null;
+    [SerializeField] Rigidbody2D ballRB = null;
+    [SerializeField] float speed = 3f;
+    [SerializeField] float xVelocity = 2f;
+    [SerializeField] float yVelocity = 15f;
+    private Vector2 paddleToBallVector;
+
+    private bool isGameStarted = false;
+    private int mousePressed = 0;
+
+    private void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        LockBallToPaddle();
+        LaunchBallAtMouseClick();
+    }
+
+    private void LockBallToPaddle()
+    {
+        if (!isGameStarted)
+        {
+            Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+            transform.position = paddlePos + paddleToBallVector;
+        }
+    }
+
+    private void LaunchBallAtMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0) && mousePressed == 0)
+        {
+            mousePressed++;
+            isGameStarted = true;
+            ballRB.simulated = true;
+            ballRB.velocity += new Vector2(xVelocity, yVelocity);
+        }
     }
 }
+// TODO: Need to set simulated variable to true when launching the ball
